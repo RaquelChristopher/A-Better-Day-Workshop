8c0fea12f089d11764f9d5dc0763caf7158f3c94
SpiritAndPollen <jrtt212@gmail.com>
2025-07-30 10:18:49 +1000

    Initial Commit

---
+++ A Better Day Workshop/A_Better_Day_WorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Better_Day_WorkshopApp.swift
+//  A Better Day Workshop
+//
+//  Created by Jedda Tuuta on 30/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct A_Better_Day_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
